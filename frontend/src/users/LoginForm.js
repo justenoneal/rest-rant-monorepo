32a5675bb88f6ec8d2ece792b21ab65e66f8143d
justenoneal <justenoneal@gmail.com>
2023-07-26 12:43:28 -0700

    Update LoginForm.js

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -29,7 +29,6 @@ function LoginForm() {
 
         if (response.status === 200) {
             setCurrentUser(data.user)
-            console.log(data.token)
             localStorage.setItem('token', data.token)
             history.push(`/`)
         } else {
